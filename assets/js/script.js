de62906302c180d93dedf63ab5824cd2548ef7ce
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-03-20 18:08:34 +1100

    adding back original code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// This creates a var called generateBtn targeted at #generate under card-footer 
+var generateBtn = document.querySelector("#generate");
+
+// creates a function called writePassword
+function writePassword() {
+
+  // this is the function in which all the alert prompts and user input occurs 
+  function generatePassword() {}
+  // this displays the generated password 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
